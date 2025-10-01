132bd6fc459a3621aaddc6734feb53301cf804ad
Jiajie-Lin23 <1308246105@qq.com>
2025-10-01 22:31:25 +0800

    Update CoreTreeLabeling.cpp

--- CoreTreeLabeling.cpp
+++ CoreTreeLabeling.cpp
@@ -3,8 +3,7 @@
 
 #include <map>
 
-
-void query_dis(string path, string graph_name, int max_w, int numLand, string bin_path)
+void query_dis(string path, string graph_name, int max_w, int numLand, bool flag, string bin_path)
 {
 	CoreTree ct(path);
 	ct.load_label(max_w, bin_path, numLand);
@@ -13,7 +12,7 @@ void query_dis(string path, string graph_name, int max_w, int numLand, string bi
 	ct.Compute_group();
 	ct.create_landmark();
 	ct.Merge_root_dis();
-	ct.compute_ecc();
+	ct.compute_ecc(flag);
 	int max_dis = 0, min_dis = INT_MAX;
 	int r = INT_MAX;
 
@@ -27,7 +26,7 @@ void query_dis(string path, string graph_name, int max_w, int numLand, string bi
 	printf("Computing ED time:%f sec", get_time() - tm);
 	ct.txt_to_file(graph_name);
 }
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	setvbuf(stderr, NULL, _IONBF, 0);
@@ -36,35 +35,36 @@ int main(int argc, char* argv[])
 	{
 		if (strcmp(argv[1], "txt-to-bin") == 0)
 		{
-			CoreTree::create_bin(argv[2], argv[3],								 /*path*/
-				argc > 4 ? atoi(argv[4]) : 1,			 /*rank_threads*/
-				argc > 5 ? atoi(argv[5]) : RANK_STATIC, /*rank_method*/
-				argc > 6 ? atoi(argv[6]) : 60,			 /*rank_max_minutes*/
-				argc > 7 ? atoi(argv[7]) : 3,			 /*max_hops*/
-				argc > 8 ? atoi(argv[7]) : false,
-				bin_path);
+			CoreTree::create_bin(argv[2], argv[3],						 /*path*/
+								 argc > 4 ? atoi(argv[4]) : 1,			 /*rank_threads*/
+								 argc > 5 ? atoi(argv[5]) : RANK_STATIC, /*rank_method*/
+								 argc > 6 ? atoi(argv[6]) : 60,			 /*rank_max_minutes*/
+								 argc > 7 ? atoi(argv[7]) : 3,			 /*max_hops*/
+								 argc > 8 ? atoi(argv[7]) : false,
+								 bin_path);
 		}
 		else if (strcmp(argv[1], "decompose_tree") == 0)
 		{
 			CoreTree ct(argv[2]);							/*path*/
 			ct.decompose_tree(atoi(argv[3]),				/*max_w*/
-				argc > 4 ? atoi(argv[4]) : 1, /*n_threads*/
-				bin_path);
+							  argc > 4 ? atoi(argv[4]) : 1, /*n_threads*/
+							  bin_path);
 		}
 		else if (strcmp(argv[1], "decompose_bt") == 0)
 		{
-			CoreTree ct(argv[2]); /*path*/
+			CoreTree ct(argv[2]);							/*path*/
 			ct.decompose_tree(atoi(argv[3]),				/*max_w*/
-				argc > 4 ? atoi(argv[4]) : 1, /*n_threads*/
-				bin_path);
+							  argc > 4 ? atoi(argv[4]) : 1, /*n_threads*/
+							  bin_path);
 			ct.save_tmp_graph(atoi(argv[3]), bin_path);
 		}
 		else if (strcmp(argv[1], "query-dis") == 0)
-			query_dis(argv[2],		  /*path*/
-				argv[3],
-				atoi(argv[4]), /*max_w*/
-				atoi(argv[5]), /*n_pairs*/
-				bin_path);
+			query_dis(argv[2], /*path*/
+					  argv[3],
+					  atoi(argv[4]), /*max_w*/
+					  atoi(argv[5]), /*n_pairs*/
+					  atoi(argv[6]),
+					  bin_path);
 	}
 
 	return 0;
